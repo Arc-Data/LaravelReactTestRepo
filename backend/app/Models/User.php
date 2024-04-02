7b8365fc8c8bd1d76ee4363a37674efe0239247f
Arc <arclight04@gmail.com>
2024-04-02 15:39:21 +0800

    jwt auth installation setups

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,11 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -43,5 +43,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    
+    public function getJWTIdentifier() 
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() 
+    {
+        return [];
     }
 }
