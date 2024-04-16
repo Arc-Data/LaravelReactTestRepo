b5497cc6a0c279f1bcd83b814e4cf58268669363
Arc <arclight04@gmail.com>
2024-04-16 11:46:24 +0800

    base post button designs

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,11 +1,27 @@
 import RelativeTime from 'dayjs/plugin/relativeTime'
 import dayjs from "dayjs"
 import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMessage, faRetweet, faShare, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { useContext } from 'react'
+import NotificationContext from '../context/NotificationContext'
+
 
 dayjs.extend(RelativeTime)
 
 const Post = ({ post }) => {
     const navigate = useNavigate()
+    const { addNotification } = useContext(NotificationContext)
+
+    const handleSubmitLike = (e) => {
+        e.stopPropagation()
+        addNotification("Post liked.")        
+    }
+
+    const handleRepost = (e) => {
+        e.stopPropagation()
+        addNotification("Post reposted")
+    }
 
     return (
         <div 
@@ -27,7 +43,21 @@ const Post = ({ post }) => {
                 <span>{dayjs(post.created_at).fromNow()}</span>
             </div>
             <p className='text-2xl font-bold group-hover:text-primary'>{post.title}</p>
-            <p className='mt-4' dangerouslySetInnerHTML={{ __html: post.description}} />
+            <p className='mt-2' dangerouslySetInnerHTML={{ __html: post.description}} />
+            <div className='flex gap-4 mt-2'>
+                <button className='flex items-center gap-4 px-2 py-2 shadow-md group/likes bg-opacity-10 bg-primary rounded-xl' onClick={handleSubmitLike}>
+                    <FontAwesomeIcon icon={faThumbsUp}  className='text-black text-opacity-40 group-hover/likes:text-primary'/>
+                    <p className='text-sm text-slate-400'>100</p>
+                </button>
+                <button className='flex items-center gap-4 px-2 py-2 shadow-md group/likes bg-opacity-10 bg-primary rounded-xl'>
+                    <FontAwesomeIcon icon={faMessage}  className='text-black text-opacity-40 group-hover/likes:text-primary'/>
+                    <p className='text-sm text-slate-400'>100</p>
+                </button>
+                <button className='flex items-center gap-4 px-2 py-2 shadow-md group/likes bg-opacity-10 bg-primary rounded-xl' onClick={handleRepost}>
+                    <FontAwesomeIcon icon={faRetweet}  className='text-black text-opacity-40 group-hover/likes:text-primary'/>
+                    <p className='text-sm text-slate-400'>100</p>
+                </button>
+            </div>
         </div>
     )
 }
